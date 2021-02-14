9cbe06f90e9eb40b168c8e4f7447d04b12ed6b73
Daniel Venera <daniel.venera.programmer@gmail.com>
2021-02-14 12:53:24 +0100

    user

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,18 @@
-//express-rate-limit
-const rateLimit = require("express-rate-limit");
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, 
-  max: 100 
-});
-//express
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-/////USE///////
-//CORS
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
   next();
 });
-//express-rate-limit
-app.use(limiter);
-app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// set port, listen for requests
+app.listen(3000, () => {
+  console.log("Server is running on port 3000.");
 });
-module.exports = app;
+require("././routes/user.routes.js")(app);
