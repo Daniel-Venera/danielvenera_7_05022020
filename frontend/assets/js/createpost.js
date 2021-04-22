33677fabc90843ee6537df1f26093daca368b28c
Daniel Venera <daniel.venera.programmer@gmail.com>
2021-04-22 10:19:41 +0200

    Update createpost.js

--- frontend/assets/js/createpost.js
+++ frontend/assets/js/createpost.js
@@ -31,19 +31,6 @@ var app = new Vue({
                 reader.readAsDataURL(files[i]);
             }
         },
-        // onSelect: function() {
-        //     const file = this.$refs.file.files[0];
-        //     if (file.type == "image/png") {
-        //         var type = "png";
-        //     } else {
-        //         var type = "jpg";
-        //     }
-        //     this.date = Date.now();
-        //     this.file = file;
-        //     // this.file.name = this.file.name.split(" ").join("_") + this.date;
-        //     const formData = new FormData();
-        //     formData.append("file", this.file);
-        // },
         async onSubmit() {
             const file = this.$refs.file.files[0];
             this.file = file;
@@ -85,44 +72,5 @@ var app = new Vue({
                 })
                 .catch(err => console.error(err));
         }
-        // async onSubmit() {
-        //     var self = this;
-        //     if (this.file) {
-        //         const formData = new FormData();
-        //         formData.append("file", this.file);
-        //         try {
-        //             await axios.post("http://localhost:3000/upload", formData).then(function(response) {
-        //                 self.fileName = response;
-        //             });
-        //         } catch (err) {
-        //         }
-        //         if (this.file.type == "image/png") {
-        //             var type = "png";
-        //         } else {
-        //             var type = "jpg";
-        //         }
-        //         this.file = "http://localhost:3000/uploads/" + this.fileName;
-        //     }
-        //     this.createPostInfos = { post_title: this.post_title, post_content: this.post_content, user_id: this.user_id, post_state: this.post_state, post_file: this.file };
-        //     this.options = {
-        //         method: "post",
-        //         body: JSON.stringify(this.createPostInfos),
-        //         headers: {
-        //             "Content-Type": "application/json"
-        //         }
-        //     };
-        //     fetch(this.urlApi, this.options)
-        //         .then(function(response) {
-        //             return response.json();
-        //         })
-        //         .then(function(response) {
-        //             // if (response.post_state == 1) {
-        //             //     location.href = "index.html?post_created=admin";
-        //             // } else {
-        //             //     location.href = "index.html?post_created=user";
-        //             // }
-        //         })
-        //         .catch(err => console.error(err));
-        // }
     }
 });
